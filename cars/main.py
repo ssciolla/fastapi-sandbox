84d48b454585cc7f038fd29cea4e3a465bf61d41
Sam Sciolla <ssciolla@umich.edu>
2023-04-26 14:35:00 -0400

    Add database persistence with sqlmodel, sqlalchemy, sqlite

--- cars/main.py
+++ cars/main.py
@@ -1,68 +1,87 @@
-from enum import Enum
-# from typing import Annotated
+import logging
 
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import Depends, FastAPI, HTTPException
+from sqlmodel import Session, select
 
-
-class FuelType(Enum):
-    electric = 'electric'
-    gas = 'gas'
-    hybrid = 'hybrid'
-    plug_in_hybrid = 'plug-in hybrid'
+from models import (
+    Brand, BrandCreate, BrandRead, BrandReadWithCars,
+    Car, CarCreate, CarRead, CarReadWithBrand,
+    ENGINE, set_up_database
+)
 
 
-class Brand(BaseModel):
-    name: str
-    founded: int
-
-
-class EntityWithID(BaseModel):
-    id: int
-
-
-class Car(BaseModel):
-    brand: Brand
-    model: str
-    fuel_type: FuelType
-
-
-class CarWithID(Car, EntityWithID):
-    pass
-
-
-CARS: list[CarWithID] = []
-
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
+# Dependencies
+
+def get_db_session():
+    with Session(ENGINE) as session:
+        yield session
+
+
 # Routes
-@app.post("/cars/")
-async def create_car(car: Car) -> CarWithID:
-    if len(CARS) > 0:
-        next_id = (max([car.id for car in CARS]) + 1)
-    else:
-        next_id = 0
-    car_with_id = CarWithID(
-        id=next_id,
-        brand=car.brand,
-        model=car.model,
-        fuel_type=car.fuel_type
-    )
-    CARS.append(car_with_id)
-    return car_with_id
 
 
-@app.get("/cars/")
-async def get_cars() -> list[CarWithID]:
-    return CARS
+@app.on_event('startup')
+def on_startup():
+    set_up_database()
 
 
-@app.delete("/cars/")
-async def delete_car(car_id: int):
-    new_cars = [car for car in CARS if car.id != car_id]
-    if len(new_cars) < len(CARS):
-        CARS = new_cars
-        return
-    raise HTTPException(404, 'no car with that ID')
+@app.post("/brands/", response_model=BrandRead)
+async def create_brand(brand: BrandCreate, session: Session = Depends(get_db_session)):
+    db_brand = Brand.from_orm(brand)
+    session.add(db_brand)
+    session.commit()
+    logger.debug(db_brand)
+    session.refresh(db_brand)
+    return db_brand
+
+
+@app.get("/brands/", response_model=list[BrandReadWithCars])
+async def get_brands(session: Session = Depends(get_db_session)):
+    brands = session.exec(select(Brand)).all()
+    return brands
+
+
+@app.delete("/brands/{brand_id}")
+async def delete_brand(brand_id: int, session: Session = Depends(get_db_session)):
+    statement = select(Brand).where(Brand.id == brand_id)
+    results = session.exec(statement)
+    brand = results.first()
+    logger.debug(brand)
+    if brand is None:
+        raise HTTPException(404, 'no brand with that ID')
+    session.delete(brand)
+    session.commit()
+
+
+@app.post("/cars/", response_model=CarRead)
+async def create_car(car: CarCreate, session: Session = Depends(get_db_session)) -> Car:
+    db_car = Car.from_orm(car)
+    session.add(db_car)
+    session.commit()
+    logger.debug(db_car)
+    session.refresh(db_car)
+    return db_car
+
+
+@app.get("/cars/", response_model=list[CarReadWithBrand])
+async def get_cars(session: Session = Depends(get_db_session)):
+    cars = session.exec(select(Car)).all()
+    return cars
+
+
+@app.delete("/cars/{car_id}")
+async def delete_car(car_id: int, session: Session = Depends(get_db_session)):
+    statement = select(Car).where(Car.id == car_id)
+    results = session.exec(statement)
+    car = results.first()
+    logger.debug(car)
+    if car is None:
+        raise HTTPException(404, 'no car with that ID')
+    session.delete(car)
+    session.commit()
+    return
---
+++ cars/models.py
@@ -0,0 +1,79 @@
+from enum import Enum
+
+from typing import Optional
+from sqlmodel import create_engine, Field, Relationship, SQLModel
+
+
+# Brand
+
+class BrandBase(SQLModel):
+    name: str = Field(index=True)
+
+
+class Brand(BrandBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    cars: list['Car'] = Relationship(back_populates='brand')
+
+
+class BrandCreate(BrandBase):
+    pass
+
+
+class BrandRead(BrandBase):
+    id: int
+
+
+# Car
+
+class FuelType(Enum):
+    electric = 'electric'
+    gas = 'gas'
+    hybrid = 'hybrid'
+    plug_in_hybrid = 'plug-in hybrid'
+
+
+class CarBase(SQLModel):
+    model: str
+    fuel_type: FuelType
+    brand_id: Optional[int] = Field(default=None, foreign_key='brand.id')
+
+
+class Car(CarBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    brand: Optional[Brand] = Relationship(back_populates='cars')
+
+
+class CarCreate(CarBase):
+    brand_id: int
+
+
+class CarRead(CarBase):
+    id: int
+    brand_id: int
+
+
+# Multiple models
+
+class CarReadWithBrand(CarRead):
+    brand: Optional[BrandRead] = None
+
+
+class BrandReadWithCars(BrandRead):
+    cars: list[CarRead] = []
+
+
+# DB setup
+
+sqlite_file_name = 'cars.db'
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {'check_same_thread': False}
+ENGINE = create_engine(sqlite_url, echo=True, connect_args=connect_args)
+
+
+def set_up_database():
+    SQLModel.metadata.create_all(ENGINE)
+
+
+if __name__ == '__main__':
+    set_up_database()
